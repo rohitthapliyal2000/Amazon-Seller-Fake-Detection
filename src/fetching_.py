7ac1f6dd5bba1f4da44484d4715f894237436045
Rohit Thapliyal <rohitthapliyal2000@geu.ac.in>
2018-04-29 01:49:52 +0530

    Fetching reviews for fake classifier

--- src/fetching_.py
+++ src/fetching_.py
@@ -7,11 +7,11 @@ def fetching(fake_product_reviews, original_product_reviews):
 	print("Phase 1...\n")
 	pb.progress(0.0, 100)
 
-	with open("dataset/fake_pro.txt", "r") as file:
+	with open("../dataset/fake_pro.txt", "r") as file:
 		for line in file:
 			fake_product_reviews.append(line)
 
-	with open("dataset/original_pro.txt", "r") as file:
+	with open("../dataset/original_pro.txt", "r") as file:
 		for line in file:
 			original_product_reviews.append(line)
 
