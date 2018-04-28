6e64ff34cf364c2b4a1a7ff54f9d66a496acaa2b
Rohit Thapliyal <rohitthapliyal2000@geu.ac.in>
2018-04-29 01:50:43 +0530

    Fetching reviews for sentiment classifier

--- src/fetching__.py
+++ src/fetching__.py
@@ -7,14 +7,14 @@ def fetching(neg_rev, pos_rev):
 	time.sleep(1)
 	pb.progress(20.0, 100.0)
 
-	with open("dataset/neg.txt") as file:
+	with open("../dataset/neg.txt") as file:
 		for line in file:
 			neg_rev.append(line.lower())
 
 	time.sleep(1)
 	pb.progress(40.0, 100.0)
 
-	with open("dataset/pos.txt") as file:
+	with open("../dataset/pos.txt") as file:
 		for line in file:
 			pos_rev.append(line.lower())
 
