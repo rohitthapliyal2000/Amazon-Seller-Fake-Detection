01242b5e3d46ffd14a164532dd66d3bded70b776
Rohit Thapliyal <rohitthapliyal2000@geu.ac.in>
2018-04-29 02:01:25 +0530

    Tracking the undergoing process

--- src/progress_bar.py
+++ src/progress_bar.py
@@ -2,7 +2,7 @@ import sys
 
 def progress(count, total, cond=False):
 
-	bar_len = 100
+	bar_len = 60
 	filled_len = int(round(bar_len * count / float(total)))
 
 	percents = round(100.0 * count / float(total), 1)
